fa51ff235208e634699d454d56b9b1f6c8466237
kmj <zkxm_0@naver.com>
2025-03-15 21:42:28 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/kmj/portfolio/PortfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.kmj.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortfolioApplication>(*args)
+}
---
+++ src/test/kotlin/com/kmj/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kmj.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
